a95d80f1f5ceb09f0bf50f3b80fb0e23d95e1a77
Umar Habib <umarhabib464@gmail.com>
2021-12-15 16:42:33 +0500

    quiz UI

---
+++ app.js
@@ -0,0 +1,12 @@
+var arrayofobj = [
+    {
+        num: 1,
+        question: "HTML stand for",
+        options: {
+          a: "Hyper Text Markup Language",
+          b: "Hyper Text Programming Language",
+          c: "Hyper Text Styling Language",
+          d: "Hyper Text Scripting Language",
+        }
+    }
+]
